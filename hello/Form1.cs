cf3f6b900a66f2064e35820d290e72d937659b59
nusaeiwyj <nusaei@qq.com>
2019-01-19 18:53:08 +0800

    hotfix ok

--- hello/Form1.cs
+++ hello/Form1.cs
@@ -20,6 +20,7 @@ namespace hello
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("HelloWorld 23334 ");//DEV2
+            //hotfix ok
         }
     }
 }
